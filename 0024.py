f380c900e1362defb689225f7c47ab6cf6d49814
nimaaskarian <nimaaskarian.work@outlook.com>
2024-11-16 09:39:00 +0330

    Update 24

--- 0024.py
+++ 0024.py
@@ -2,4 +2,4 @@ import itertools
 
 iterator = itertools.permutations("0123456789", 10)
 milionth_permutation = next(x for i,x in enumerate(iterator) if i==999999)
-print(milionth_permutation)
+print("".join(milionth_permutation))
