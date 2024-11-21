f0c12d25ae8b75fc0b5547e45e235770c5b3d2c8
nimaaskarian <nimaaskarian.work@outlook.com>
2024-11-21 14:00:36 +0330

    Update 39 to make it faster

--- 0039.py
+++ 0039.py
@@ -2,18 +2,12 @@ import math
 
 solutions = dict()
 for a in range(1, 1000):
-    for b in range(a, 1000):
-        if a+b > 1000:
-            break
-        c, mod = divmod(math.sqrt(a**2+b**2), 1)
-        if mod != 0:
+    for b in range(a, 1000-a):
+        if (c := math.sqrt(a**2+b**2)) != int(c):
             continue
         p = a+b+int(c)
         if p > 1000:
             break
-        if solutions.get(p):
-            solutions[p] +=1
-        else:
-            solutions[p] = 1
+        solutions[p] = solutions.get(p, 0) + 1
 
 print(max(solutions, key=solutions.get))
