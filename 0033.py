17b7c62f08db44f34f0cc39650c0df06df15b87f
nimaaskarian <nimaaskarian.work@outlook.com>
2024-11-21 08:19:25 +0330

    Add updated smort way of 33

--- 0033.py
+++ 0033.py
@@ -1,27 +1,11 @@
-from itertools import permutations
-from mymath import lowest_common_terms
-from utils import s_iter2int
+from itertools import product
+from math import prod
+from fractions import Fraction
 
-base = "123456789"
-deno_all = 1
-numn_all = 1
-for sdeno in permutations(base, 2):
-    for snumn in permutations(base, 2):
-        if sdeno <= snumn:
-            continue
-        common = (ch for ch in sdeno if ch in snumn)
-        if not any(common):
-            continue
-        if sdeno == snumn[::-1]:
-            continue
-        su_deno = s_iter2int(ch for ch in sdeno if ch not in snumn)
-        su_numn = s_iter2int(ch for ch in snumn if ch not in sdeno)
+def digit_cancelling_fractions():
+    for d in range(2, 10):
+        for n, m in product(range(1, d), range(d+1, 10)):
+            if Fraction(n,d) == Fraction(n*10+m,d+10*m):
+                yield Fraction(n, d)
 
-        deno = s_iter2int(sdeno)
-        numn = s_iter2int(snumn)
-
-        if lowest_common_terms(su_numn,su_deno) == lowest_common_terms(numn, deno):
-            deno_all*=su_deno
-            numn_all*=su_numn
-
-print(lowest_common_terms(deno_all, numn_all)[0])
+print(prod(digit_cancelling_fractions()).denominator)
